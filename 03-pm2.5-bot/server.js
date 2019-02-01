632ca3451d082b6730d36897adb6dd2af83e5368
Nitipat L <iamsvz@gmail.com>
2019-02-01 16:39:14 +0700

    [Edited] bot

--- 03-pm2.5-bot/server.js
+++ 03-pm2.5-bot/server.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const line = require('@line/bot-sdk');
+const express = require('express')
+const line = require('@line/bot-sdk')
+const restClient = new (require('node-rest-client').Client)
 
-require('dotenv').config();
+const apiUrl = 'https://fathomless-reaches-36581.herokuapp.com/api'
+
+require('dotenv').config()
 const app = express()
 
 const config = {
@@ -12,30 +15,60 @@ const config = {
 const client = new line.Client(config);
 
 app.get('/', function (req, res) {
-	res.send('03-pm2.5-bot');
+	res.send('03-pm2.5-bot')
 })
 
 app.post('/webhook', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
+    .catch(err => console.log('err', err))
 });
 
 function handleEvent(event) {
-  console.log(event);
-  if(event.type === 'message' && event.message.type === 'sticker') {
-    handleMessageEvent(event);
+  if(event.type === 'message' && event.message.type === 'location') {
+    return handleLocationEvent(event)
   }else {
     return Promise.resolve(null)
   }
 }
 
-function handleMessageEvent(event) {
-  var msg = {
-    type: 'text',
-    text: 'This is sticker'
-  }
-  return client.replyMessage(event.replyToken, msg);
+function handleLocationEvent(event) {
+  return new Promise((resolve, reject) => {
+    restClient.get(`${apiUrl}?lat=${event.message.latitude}&long=${event.message.longitude}`, (data, response) => {
+      if (data) {
+        const pinData = data.map(row => ({
+          "thumbnailImageUrl": row.aqi.icon,
+          "imageBackgroundColor": "#FFFFFF",
+          "title": `PM 2.5: ${row.aqi.aqi}`,
+          "text": `${row.nameTH}, ${row.areaTH}`,
+          "actions": [
+            {
+              "type": "uri",
+              "label": "ข้อมูลย้อนหลัง",
+              "uri": row.historyUrl
+            }
+          ]
+        }))
+    
+        var msg = {
+          "type": "template",
+          "altText": "ข้อมูลสถานที่",
+          "template": {
+            "type": "carousel",
+            "columns": pinData,
+            "imageAspectRatio": "rectangle",
+            "imageSize": "cover"
+          }
+        }
+
+        resolve(client.replyMessage(event.replyToken, msg))
+      } else {
+        reject()
+      }
+    })
+  })
+ 
 }
 
 app.set('port', (process.env.PORT || 4000))
